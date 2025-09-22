9f393a7db648a29e82b2d5bc61ed070a2e740cd5
hongsik.yoo <hongsik.yoo@bankwareglobal.com>
2025-09-22 17:24:02 +0900

    init project

---
+++ src/routes/main/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/main/')({
+  component: App,
+})
+
+function App() {
+  return <div className="text-center">main page</div>
+}
