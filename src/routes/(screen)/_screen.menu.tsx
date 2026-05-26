d7ae306e0e98e8eba1ddc2447eecc394d61c9e5e
hongsik.yoo <hongsik.yoo@bankwareglobal.com>
2026-05-26 10:02:18 +0900

    .

--- src/routes/(screen)/_screen.menu.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import ScreenMenu from '@/pages/page/menu';
-
-export const Route = createFileRoute('/(screen)/_screen/menu')({
-  component: ScreenMenu,
-});
